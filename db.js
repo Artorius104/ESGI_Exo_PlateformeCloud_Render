b1d0dce548d7059a4493910d480339b916933be4
Divin <72012711+Artorius104@users.noreply.github.com>
2024-11-29 17:35:43 +0100

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 // Database
 const sequelize = new Sequelize(
-  '', // TODO: database connection string
+  'postgresql://esgi_plateformecloud_exo_render_db_user:AJl3IyyuXmnSd1Au5Y54OrFVg3Hwt9cL@dpg-ct4uo156l47c73fbj2vg-a/esgi_plateformecloud_exo_render_db', // TODO: database connection string
   {
     dialect: 'postgres',
     dialectOptions: {
